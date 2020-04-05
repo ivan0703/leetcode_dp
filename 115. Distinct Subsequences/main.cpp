ad36fc0e18140ba5b535d06de16fa08f32c388cb
ivan chung <ivan0703@gmail.com>
2020-04-06 01:43:22 +0800

    115. [c++] Distinct Subsequences

---
+++ 115. Distinct Subsequences/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int M = s.length();
+        int N = t.length();
+        
+        vector<vector<long long>> dp(M+1, vector<long long>(N+1,0));
+        for(int i=0; i<=M; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<=M; i++) {
+            for(int j=1; j<=N; j++) {
+                if(s[i-1]==t[j-1]) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[M][N];
+    }
+};
+
+int main()
+{
+    //string S = "rabbbit";
+    //string T = "rabbit";
+    string S = "xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo";
+    string T = "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys";
+    Solution sol;
+    cout<<sol.numDistinct(S,T)<<endl;
+
+    return 0;
+}
