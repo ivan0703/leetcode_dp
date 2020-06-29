9c8f36a5226a57f7bcefc96d5f513768716ec927
ivan <ivan0703@gmail.com>
2020-06-29 23:27:09 +0800

    300. Longest Increasing Subsequence - 96.64

--- 300. Longest Increasing Subsequence/main.py
+++ 300. Longest Increasing Subsequence/main.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -15,6 +16,19 @@ class Solution:
             res = max(res, dp[i])
         return res
 
+    def lengthOfLISv2(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        res = []
+        for i in range(len(nums)):
+            p = bisect.bisect_left(res, nums[i])
+            if p == len(res):
+                res.append(nums[i])
+            else:
+                res[p] = nums[i]
+        return len(res)
+
 if __name__ == "__main__":
     sol = Solution()
     print(sol.lengthOfLIS([10,9,2,5,3,7,101,18]))
+    print(sol.lengthOfLISv2([10,9,2,5,3,7,101,18]))
