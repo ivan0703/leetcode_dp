c1478971db6bea56795531b0b41985ad8189e699
ivan <ivan0703@gmail.com>
2020-08-07 00:29:29 +0800

    1235. Maximum Profit in Job Scheduling (17.48%)

--- 1235. Maximum Profit in Job Scheduling/main.py
+++ 1235. Maximum Profit in Job Scheduling/main.py
@@ -1,29 +1,25 @@
 from typing import List
-from functools import cmp_to_key
+from operator import itemgetter
+import bisect
 
 class Solution:
-    def mycmp(self, t1, t2):
-        if t1[0] != t2[0]:
-            return t1[0] - t2[0]
-        elif t1[1] != t2[1]:
-            return t1[1] - t2[1]
-        else:
-            return t2[2] - t1[2]
 
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         h = len(startTime)
         jobs = []
         for i in range(h):
             jobs.append((startTime[i],endTime[i],profit[i]))
-        jobs.sort(key=cmp_to_key(self.mycmp))
+        # sort by endTime
+        jobs.sort(key=itemgetter(1))
+        ends = [x[1] for x in jobs]
 
-        dp = [0] * h
-        for i in range(h-1, -1, -1):
-            dp[i] = jobs[i][2]
-            for j in range(i+1, h):
-                if jobs[j][0] >= jobs[i][1]:
-                    dp[i] = max(dp[i], jobs[i][2] + dp[j])
-        return max(dp)
+        dp = [jobs[0][2]] + [0] * (h-1)
+        for i in range(1,h):
+            idx = bisect.bisect_right(ends, jobs[i][0], 0, i)
+            pre = 0 if idx==0 else dp[idx-1]
+            dp[i] = max(pre+jobs[i][2], dp[i-1])   
+
+        return dp[-1]
 
 if __name__ == "__main__":
     
