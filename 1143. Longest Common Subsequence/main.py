89fe3c328e01d7a31fc3dcc8d24179c234d668e4
ivan <ivan0703@gmail.com>
2020-08-21 00:44:02 +0800

    1143. Longest Common Subsequence (python, 68.23%)

--- 1143. Longest Common Subsequence/main.py
+++ 1143. Longest Common Subsequence/main.py
@@ -4,11 +4,10 @@ class Solution:
         dp = [[0 for _ in range(N+1)] for _ in range(M+1)]
         for i in range(1, M+1):
             for j in range(1,N+1):
-                dp[i][j] = dp[i-1][j-1]
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] += 1
-                dp[i][j] = max(dp[i][j], dp[i-1][j])
-                dp[i][j] = max(dp[i][j], dp[i][j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
         return dp[M][N]
 
 if __name__ == "__main__":
